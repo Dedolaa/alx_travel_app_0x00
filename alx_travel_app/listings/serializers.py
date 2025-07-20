e5b886baea6c4802569f243b6178532a5bc5057b
Dedolaa <adedolapobakare16@gmail.com>
2025-07-20 18:54:31 +0100

    Update serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -5,3 +5,16 @@ class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = '__all__'
+
+from rest_framework import serializers
+from .models import Listing, Booking
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
